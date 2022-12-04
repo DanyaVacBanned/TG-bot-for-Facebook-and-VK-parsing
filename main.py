50df0bda15b1e734a31d0e8a5175558f6ffd239f
Danil <90012018+DanyaVacBanned@users.noreply.github.com>
2022-12-04 14:15:35 +0300

    Update main.py
    TODO:
    VK and TG parsers

--- main.py
+++ main.py
@@ -1,56 +1,99 @@
+from time import sleep
 from aiogram import Bot, Dispatcher, executor, types
 import logging
-from cfg import get_token, get_admin_id, get_chat_id, tmp
-from parseFile import parsing, get_urls
+from cfg import get_admin_id, get_chat_id, get_channel_id
+from parseFile import parsing
+from kb import main_bot_keyboard
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.dispatcher import FSMContext
-from FSM import Groups
+from database import Database
+# from vk_parsing import vk_parsing_func
+import configparser
+from asyncio.exceptions import TimeoutError
+import asyncio
 #Initializing
+config = configparser.ConfigParser()
+config.read('config.ini')
 logging.basicConfig(level=logging.INFO)
-Token = get_token()
-storage = MemoryStorage()
+Token = config['Telegram']['first_token']
 admin_id = get_admin_id()
 bot = Bot(token=Token)
-dp = Dispatcher(bot, storage=storage)
+dp = Dispatcher(bot)
+db = Database('fb_db.db')
 
-#BODY
+#--------------------------PARSING----------------------------------------
 
-@dp.message_handler(commands=['fb_parse'])
-async def on_start(message: types.Message):
-    await bot.send_message(message.chat.id, 'Привет! Начинаю работу...')
-    for i in get_urls():
-        res = str(parsing(i))
-        if res != 'None':
-            await bot.send_message(message.chat.id, res)
+
+@dp.message_handler(commands=['Начало_парсинга'])
+async def on_start_parsing(message: types.Message):
+    fb_posts = []
     
-
-@dp.message_handler(commands=['get_id'])
-async def get_my_chat(message: types.Message):
-    await bot.send_message(message.chat.id, message.chat.id)
-
-@dp.message_handler(commands=['reg'], state=None)
-async def get_start(message: types.Message):
-    print('Бот отреагировал на сообщение')
-    await Groups.groups.set()
-    await bot.send_message(message.chat.id, 'Введите список групп: ')
-@dp.message_handler(state=Groups.groups)
-async def load_groups(message: types.Message, state=FSMContext):
-    async with state.proxy() as sp:
-        sp['groups'] = message.text
-        await Groups.next()
-        await bot.send_message(message.chat.id, "Введите код страны в двоичном формате (ru, us, de)")
-@dp.message_handler(state=Groups.country)
-async def load_country(message: types.Message, state=FSMContext):
-    async with state.proxy() as sp:
-        country = message.text
-        groups = sp['groups']
-        with open(f'groups/groups_{country}.txt', 'w', encoding='utf-8') as f:
-            f.write(groups)
-        tmp(message.text)
-    await state.finish()
+    try:
+        await bot.send_message(message.chat.id, 'Привет! Начинаю работу...')
+        for i in db.make_array_from_groups_list(message.chat.id):
+            curent_channel = db.select_channel_id_by_group(message.chat.id)
+            if i.split('/')[2] == 'facebook.com':
+                print('Начало парсинга')
+                res = parsing(i)
+                print(res)
+                if res != None:
+                    if res['text'] not in fb_posts:
+                        caption = f'Есть совпадение по слову {res["keyword"]}:\n{res["text"]}\nнайденно в группе:{res["group"]},\nЛокация: {res["place"]}\nСсылка:{res["link"]}'
+                        caption_for_channel = f'{res["text"]}\n\nЛокация: {res["place"]}'
+                        fb_posts.append(res['text'])
+                        if res['image'] != 'Изображение отсутствует':
+                            try:
+                                await bot.send_message(message.chat.id,text=caption)
+                                await bot.send_message(curent_channel,text=caption_for_channel)
+                                for pic in res['image']:
+                                    await bot.send_photo(message.chat.id, pic)
+                                    await bot.send_photo(curent_channel, pic)
+                            except:
+                                await bot.send_message(message.chat.id,text=caption)
+                                await bot.send_message(curent_channel,text=caption_for_channel)
+                        else:
+                            await bot.send_message(message.chat.id, text=caption)
+                            await bot.send_message(curent_channel,text=caption_for_channel)
         
 
+            
+                # elif i.split('/')[2] == 'vk.com':
+                #     if message.chat.type == "private":
+                #         res = vk_parsing_func(i)
+                #         if res != None:
+                #             try:
+                #                 await bot.send_photo(message.chat.id, f"{res['img']}")
+                #                 await bot.send_message(message.chat.id, f"{res['text']} \n {res['link']}")
+                #             except:
+                #                 await bot.send_message(message.chat.id, f"{res['text']} \n {res['link']}")
+                #     else:
+                #         res = vk_parsing_func(i)
+                #         if res != None:
+                #             try:
+                #                 await bot.send_photo(get_channel_id(), f"{res['img']}")
+                #                 await bot.send_message(get_channel_id(), f"{res['text']}")
+                #             except:
+                #                 await bot.send_message(get_channel_id(), f"{res['text']}")
+
+                # else:
+                #     await bot.send_message(message.chat.id, 'Ссылка указана неверно')
+                    else:
+                        await bot.send_message(message.chat.id, 'Ничего не найденно')
+    except TimeoutError:
+        pass
+
+#---------------------------START-----------------------------------------
+
+@dp.message_handler(commands=['start'])
+async def on_start(message: types.Message):
+    await bot.send_message(message.chat.id, text='Здравствуйте!, чтобы зарегистрировать группы, перейдите по этой ссылке: https://t.me/facebook_vk_tg_registrationBOT', reply_markup=main_bot_keyboard)
+    
+#----------------------------GET CURRENT CHAT ID--------------------------
+@dp.message_handler(commands=['Получить_ID'])
+async def get_my_chat(message: types.Message):
+    await bot.send_message(message.chat.id, message.chat.id)  # type: ignore
+
+    
+#-----------------------------------COMMENTS-------------------------------------------------------------------
 @dp.message_handler(content_types='text')
 async def comments(message: types.Message):
     if message.text != '/get_id' and message.text != '/fb_parse' and message.text != '/start' and message.text != '/reg': 
@@ -64,8 +107,13 @@ async def comments(message: types.Message):
                 kb.add(kb_btn)
 
                 await bot.send_message(admin_id, 'У вас новый комментарий!\n'+message.text, reply_markup=kb)
-#----------------------------------------------------------------------------
-#States
+
+
+
+    
+
+
+
 
 if __name__ =='__main__':
     executor.start_polling(dp, skip_updates=True)
