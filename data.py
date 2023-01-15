b5ac0f24ac28b343c61b1b28e62b18d5338beaba
Danil <90012018+DanyaVacBanned@users.noreply.github.com>
2023-01-15 21:55:39 +0300

    Update data.py

--- data.py
+++ data.py
@@ -1,10 +1,13 @@
-def get_words():
+def get_words(name):
 
-    with open("keywords_.txt" , 'r', encoding='utf-8') as file:
+    with open(f"Keywords dir/{name}.txt" , 'r', encoding='utf-8') as file:
         words = [row.strip() for row in file]
         
         return words
 
-
+def get_kv_words():
+    with open('keywords_kv.txt','r',encoding='utf-8') as file:
+        words = [row.strip() for row in file]
+        return words
 
 
