ed6a56e897f657352aa31dca7222e85e8777220c
Danil <90012018+DanyaVacBanned@users.noreply.github.com>
2023-01-15 21:56:23 +0300

    Update vk_parsing.py

--- vk_parsing.py
+++ vk_parsing.py
@@ -1,12 +1,11 @@
 import requests
 from configparser import ConfigParser
-import json
 from data import get_words, get_kv_words
-def vk_parsing_func(group_name):
+def vk_parsing_func(group_name, preset_name):
     config = ConfigParser()
     config.read('vk_config.ini')
     api_key = config['VK']['api_key']
-    url = f'https://api.vk.com/method/wall.get?domain={group_name}&count=30&access_token={api_key}&v=5.131'
+    url = f'https://api.vk.com/method/wall.get?domain={group_name}&count=100&access_token={api_key}&v=5.131'
     req = requests.get(url)
     src = req.json()
     posts = src['response']['items']
@@ -14,13 +13,12 @@ def vk_parsing_func(group_name):
     for post in posts:
 
         post_id = post["id"]
-        print(f"Отправляем пост с ID {post_id}")
 
         try:
                 
             post_text = post['text']
             splitted_text = post_text.lower().split()
-            for keyword in get_words():
+            for keyword in get_words(preset_name):
                 for kv_keyword in get_kv_words():
                     if keyword in splitted_text and kv_keyword in splitted_text:
 
@@ -30,14 +28,16 @@ def vk_parsing_func(group_name):
                             if post_attachments[0]["type"] == "photo":
                                 photo_sizes = post_attachments[0]['photo']['sizes']
                                 post_photo.append(photo_sizes[-1]['url'])
-                            
+                        else:
+                            post_photo = None
                             
                         return {
                             'text':post_text,
                             'photo':post_photo
                         }
-                    ]
+                        
                     
         except Exception:
                 print(f"Что-то пошло не так с постом ID {post_id}")
 
+# print(vk_parsing_func('argentina_russia'))
