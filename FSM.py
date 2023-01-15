79773c872e54f8fc2e545a6c3645e35563c2a07f
Danil <90012018+DanyaVacBanned@users.noreply.github.com>
2023-01-15 21:51:28 +0300

    Update FSM.py

--- FSM.py
+++ FSM.py
@@ -1,5 +1,16 @@
 from aiogram.dispatcher.filters.state import State, StatesGroup
 
 class Groups(StatesGroup):
-    groups = State()
-    country = State()
+    groups_list = State()
+    channel_id = State()
+
+class SetKeywords(StatesGroup):
+    current_message = State()
+    preset_name = State()
+
+class DeleteKeywords(StatesGroup):
+    preset_name = State()
+
+
+class ParsingSettings(StatesGroup):
+    preset_name = State()
