d49ab0fc670039664bc0c268a2fb97aaea286314
Danil <90012018+DanyaVacBanned@users.noreply.github.com>
2023-01-15 21:55:55 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,6 +1,3 @@
-#Планирую переписать программу под PostgreSQL
-
-
 import sqlite3
 
 class Database:
@@ -9,10 +6,74 @@ class Database:
         self.cursor = self.connection.cursor()
 
     
-    def add_groups(self, group_id, groups_list, country_id):
+    def add_new_list(self, ID, groups_list):
         with self.connection:
-            return self.cursor.execute('insert into `groups` (`group_id`, `groups_list`, `country_id`) values (?,?,?)',(group_id, groups_list, country_id,))
+            return self.cursor.execute('insert into groups (`groups_list`,`id`) values (?, ?)', (groups_list,ID,))
+        
+    def make_array_from_groups_list(self, ID):
+        with self.connection:
+            data = self.cursor.execute('select `groups_list` from `groups` where `id`=?',(ID,)).fetchall()
+            for row in data:
+                return row[0].strip().split()
+
+    # def add_new_channel_by_group(self, channel_id, group_id):
+    #     with self.connection:
+    #         return self.cursor.execute('insert into `channel_ids` (`ID`, `channel_id`) values (?,?)', (group_id, channel_id,))
     
-    def select_groups_list(self, country_id):
+    # def select_channel_id_by_group(self, group_id):
+    #     with self.connection:
+    #         data = self.cursor.execute('select channel_id from `channel_ids` where `ID` = ?', (group_id,)).fetchall()
+    #         for row in data:
+    #             return row[0]
+            
+
+    def check_groups_db_data(self):
         with self.connection:
-            return self.cursor.execute('select `groups_list`, `group_id` from `groups` where `country_id`=?', (country_id,))
+            return self.cursor.execute('select * from groups').fetchall()
+    
+    def check_channel_id_db_data(self):
+        with self.connection:
+            return self.cursor.execute("select * from channel_ids").fetchall()
+
+    def reset_all_data(self):
+        with self.connection:
+            self.cursor.execute("DELETE FROM groups")
+            self.cursor.execute("DELETE FROM channel_ids")
+            self.cursor.execute("DELETE FROM post_content")
+
+    def insert_post_content(self, text):
+        with self.connection:
+            self.cursor.execute('INSERT INTO post_content (text) VALUES (?)',(text,))
+
+    def select_post_content(self):
+        with self.connection:
+            return self.cursor.execute('SELECT * FROM post_content').fetchall()
+
+
+    def make_array_from_post_content_data(self):
+        with self.connection:
+            post_content_list = []
+            data = self.cursor.execute("SELECT * FROM post_content").fetchall()
+            for row in data:
+                post_content_list.append(row[1])
+            if post_content_list == []:
+                return ['zero']
+            return(post_content_list)
+    
+    def reset_post_content_data(self):
+        with self.connection:
+            self.cursor.execute('DELETE FROM post_content')
+    def reset_groups_data(self):
+        with self.connection:
+            self.cursor.execute("DELETE FROM groups")
+    
+    # def reset_groups_for_picked_channel(self, chatId):
+    #     with self.connection:
+    #         self.cursor.execute("DELETE FROM groups WHERE id = ?",(chatId,))
+
+    
+
+# db = Database('fb_db.db')
+
+
+# print(db.make_array_from_post_content_data())
